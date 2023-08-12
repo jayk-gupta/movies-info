2001c91914007e7e68b03e6f9bfd84649e1705f6
Jay Kumar Gupta <100681165+jayk-gupta@users.noreply.github.com>
2023-08-12 11:29:42 +0530

    https added to api link

--- src/components/SelectedMovie.jsx
+++ src/components/SelectedMovie.jsx
@@ -51,7 +51,7 @@ console.log(isWatched);
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         console.log(data);
